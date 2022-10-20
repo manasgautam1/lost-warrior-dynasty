63769ec002476cf0abf6e2c1931ac9cb1bdef7bb
manasgautam1 <69212064+manasgautam1@users.noreply.github.com>
2022-10-20 16:22:51 +0530

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div id='contact' className='footer py-lg-5 py-5'>
-        {/* <p className="connect-wallet text-center mb-lg-5 mb-5">
+        <p className="connect-wallet text-center mb-lg-5 mb-5">
             <button>Connect Wallet</button>
-        </p> */}
+        </p>
         <footer className='footer-content'>
             <div className="container-xxl px-xxl-0 px-3 py-lg-3 py-2">
             <div className="d-flex justify-content-between align-items-center">
@@ -19,7 +19,7 @@ const Footer = () => {
                             </a>
                         </div>
                         <div className="col">
-                            {/* <i className="fa-brands fa-discord"></i> */}
+                            <i className="fa-brands fa-discord"></i>
                         </div>
                     </div>
                 </div>
